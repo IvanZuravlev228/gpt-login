d8ba3ac24058a99b05b0467bc97241635f093382
IvanZuravlev228 <courses.zhuravlev.ivan@gmail.com>
2024-07-03 20:15:44 +0300

    added components folder and improve auth with google

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AuthForm from "./AuthForm/AuthForm";
-import Home from "./Home/Home";
+import AuthForm from "./components/AuthForm/AuthForm";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
